cf4121d8a53a275d71effccb77d8cecfa33f653d
Serge Karpiesz <serge.karpiesz@recruitee.com>
2020-02-05 14:04:45 +0100

    Implement consumer

---
+++ lib/lepus/consumer.ex
@@ -0,0 +1,222 @@
+defmodule Lepus.Consumer do
+  @callback options() :: Keyword.t()
+
+  alias Lepus.RabbitClient
+
+  defmacro __using__(opts) do
+    exchange = opts |> Keyword.fetch!(:exchange)
+    routing_key = opts |> Keyword.fetch!(:routing_key)
+    rabbit_client = opts |> Keyword.get(:rabbit_client, RabbitClient)
+
+    broadway_producer_module =
+      opts |> Keyword.get(:broadway_producer_module, BroadwayRabbitMQ.Producer)
+
+    strategy_data = %{
+      routing_key: routing_key,
+      exchange: exchange,
+      delay_exchange: "#{exchange}.delay",
+      retry_exchange: "#{exchange}.retry",
+      queue: "#{exchange}.#{routing_key}",
+      retry_queue: "#{exchange}.#{routing_key}.retry"
+    }
+
+    quote do
+      use Broadway
+
+      @behaviour Lepus.Consumer
+
+      def start_link(opts \\ []) do
+        Lepus.Consumer.start_link(
+          __MODULE__,
+          unquote(broadway_producer_module),
+          unquote(rabbit_client),
+          unquote(Macro.escape(strategy_data)),
+          options(),
+          opts
+        )
+      end
+
+      @impl Broadway
+      def handle_failed(messages, _) do
+        Lepus.Consumer.handle_failed(
+          unquote(rabbit_client),
+          messages,
+          unquote(strategy_data.delay_exchange)
+        )
+
+        messages
+      end
+    end
+  end
+
+  def start_link(
+        module,
+        broadway_producer_module,
+        rabbit_client,
+        %{queue: queue} = strategy_data,
+        internal_opts,
+        external_opts
+      ) do
+    connection_opts =
+      internal_opts
+      |> Keyword.get(:producer, [])
+      |> Keyword.get(:module, [])
+      |> Keyword.fetch(:connection)
+      |> case do
+        {:ok, opts} -> opts
+        _ -> external_opts |> Keyword.fetch!(:connection)
+      end
+
+    process_name =
+      external_opts
+      |> Keyword.get_lazy(:name, fn -> internal_opts |> Keyword.get(:name, module) end)
+
+    modified_opts =
+      internal_opts
+      |> Keyword.put(:name, process_name)
+      |> update_option(:producer, fn producer_opts ->
+        producer_opts
+        |> Keyword.put(:transformer, {__MODULE__, :transform, []})
+        |> update_option(:module, fn producer_module_opts ->
+          updated_producer_module_opts =
+            producer_module_opts
+            |> Keyword.put(:connection, connection_opts)
+            |> Keyword.merge(on_failure: :ack, queue: queue)
+            |> update_option(:metadata, fn metadata ->
+              [[:headers, :routing_key, :content_type, :timestamp] | metadata]
+              |> List.flatten()
+              |> Enum.uniq()
+            end)
+
+          {broadway_producer_module, updated_producer_module_opts}
+        end)
+      end)
+
+    declare_strategy(rabbit_client, connection_opts, strategy_data)
+
+    Broadway.start_link(module, modified_opts)
+  end
+
+  def transform(%{metadata: %{content_type: content_type}} = message, _) do
+    message |> Broadway.Message.update_data(&from_binary(&1, content_type))
+  end
+
+  defp update_option(keyword, key, fun) do
+    keyword |> Keyword.put_new(key, []) |> Keyword.update!(key, fun)
+  end
+
+  defp declare_strategy(rabbit_client, connection_opts, %{
+         exchange: exchange,
+         routing_key: routing_key,
+         delay_exchange: delay_exchange,
+         retry_exchange: retry_exchange,
+         queue: queue,
+         retry_queue: retry_queue
+       }) do
+    with_new_connection(rabbit_client, connection_opts, fn channel ->
+      [exchange, delay_exchange, retry_exchange]
+      |> Enum.each(&rabbit_client.declare_direct_exchange(channel, &1, durable: true))
+
+      rabbit_client.declare_queue(channel, queue, durable: true)
+
+      rabbit_client.declare_queue(channel, retry_queue,
+        durable: true,
+        arguments: [{"x-dead-letter-exchange", :longstr, retry_exchange}]
+      )
+
+      [{queue, exchange}, {queue, retry_exchange}, {retry_queue, delay_exchange}]
+      |> Enum.each(fn {q, ex} ->
+        rabbit_client.bind_queue(channel, q, ex, routing_key: routing_key)
+      end)
+    end)
+  end
+
+  def handle_failed(rabbit_client, messages, delay_exchange) do
+    with [%{acknowledger: {_, %{conn: connection}, _}} | _] <- messages do
+      with_new_channel(rabbit_client, connection, fn channel ->
+        messages |> Enum.each(&handle_failed_message(rabbit_client, &1, delay_exchange, channel))
+      end)
+    end
+
+    messages
+  end
+
+  defp handle_failed_message(
+         rabbit_client,
+         %{
+           data: data,
+           metadata:
+             %{headers: headers, routing_key: routing_key, content_type: content_type} = meta
+         },
+         delay_exchange,
+         channel
+       ) do
+    payload = data |> to_binary(content_type)
+
+    retry_number = get_header_value(headers, "x-retries", 0) + 1
+    new_headers = headers |> update_headers([{"x-retries", :long, retry_number}])
+
+    opts =
+      meta
+      |> Map.put(:headers, new_headers)
+      |> Map.put(:expiration, expiration_prop(retry_number))
+      |> Map.to_list()
+
+    rabbit_client.publish(channel, delay_exchange, routing_key, payload, opts)
+  end
+
+  defp expiration_prop(retry_number) do
+    expiration_sec = :math.pow(2, retry_number) |> Kernel.trunc()
+    "#{expiration_sec * 1000}"
+  end
+
+  defp get_header_value(headers, name, default) when is_list(headers) do
+    headers
+    |> Enum.split_with(fn
+      {^name, _, _} -> true
+      _ -> false
+    end)
+    |> case do
+      {[{_, _, value} | _], _} -> value
+      _ -> default
+    end
+  end
+
+  defp get_header_value(:undefined, _, default), do: default
+
+  defp update_headers(headers, tuples) when is_list(headers) do
+    [tuples | headers] |> List.flatten() |> Enum.uniq_by(fn {k, _, _} -> k end)
+  end
+
+  defp update_headers(:undefined, tuples), do: tuples
+
+  defp with_new_connection(rabbit_client, connection_opts, fun) do
+    {:ok, connection} = rabbit_client.open_connection(connection_opts)
+    with_new_channel(rabbit_client, connection, fun)
+    rabbit_client.close_connection(connection)
+  end
+
+  defp with_new_channel(rabbit_client, connection, fun) do
+    {:ok, channel} = rabbit_client.open_channel(connection)
+    fun.(channel)
+    rabbit_client.close_channel(channel)
+  end
+
+  defp from_binary(data, "application/json") when is_binary(data) do
+    case Jason.decode(data) do
+      {:ok, decoded_data} -> decoded_data
+      _ -> data
+    end
+  end
+
+  defp from_binary(data, _), do: data
+
+  defp to_binary(data, "application/json") do
+    case Jason.encode(data) do
+      {:ok, encoded_data} -> encoded_data
+      _ -> data
+    end
+  end
+
+  defp to_binary(data, _), do: data
+end
---
+++ test/lepus/consumer_test.exs
@@ -0,0 +1,297 @@
+defmodule Lepus.ConsumerTest do
+  use ExUnit.Case, async: true
+
+  defmodule RabbitClientMock do
+    def start_link, do: Agent.start_link(fn -> [] end)
+
+    def open_connection(pid) do
+      pid |> log(:open_connection, [pid])
+      {:ok, {:connection, pid}}
+    end
+
+    def close_connection({:connection, pid} = connection) do
+      pid |> log(:close_connection, [connection])
+    end
+
+    def open_channel({:connection, pid}) do
+      pid |> log(:open_channel, [{:connection, pid}])
+      {:ok, {:channel, System.unique_integer(), pid}}
+    end
+
+    def close_channel({:channel, _, pid} = channel) do
+      pid |> log(:close_channel, [channel])
+    end
+
+    def declare_queue({:channel, _, pid} = channel, queue, opts) do
+      pid |> log(:declare_queue, [channel, queue, opts])
+    end
+
+    def bind_queue({:channel, _, pid} = channel, queue, exchange, opts) do
+      pid |> log(:bind_queue, [channel, queue, exchange, opts])
+    end
+
+    def declare_direct_exchange({:channel, _, pid} = channel, exchange, opts) do
+      pid |> log(:declare_direct_exchange, [channel, exchange, opts])
+    end
+
+    def publish({:channel, _, pid} = channel, exchange, routing_key, payload, opts) do
+      pid |> log(:publish, [channel, exchange, routing_key, payload, opts])
+    end
+
+    defp log(pid, name, args) do
+      pid |> Agent.update(fn state -> [{name, args} | state] end)
+    end
+
+    def get_log(pid), do: pid |> Agent.get(& &1) |> Enum.reverse()
+  end
+
+  defmodule BroadwayProducerMock do
+    use GenStage
+
+    def push_message(producer, data, metadata \\ %{}, rabbit_client \\ nil) do
+      metadata =
+        metadata
+        |> Map.put(:test_pid, self())
+        |> Map.put_new(:content_type, :undefined)
+        |> Map.put_new(:headers, :undefined)
+        |> Map.put_new(:routing_key, :undefined)
+        |> Map.put_new(:timestamp, :undefined)
+
+      acknowledger = {BroadwayProducerMock, %{conn: {:connection, rabbit_client}}, :ok}
+      message = %Broadway.Message{data: data, metadata: metadata, acknowledger: acknowledger}
+
+      producer |> GenStage.cast({:push_message, message})
+    end
+
+    def init(init_arg), do: {:producer, init_arg}
+    def handle_demand(_demand, init_arg), do: {:noreply, [], init_arg}
+
+    def ack(_, successful, failed) do
+      [successful: successful, failed: failed]
+      |> Enum.each(fn {k, messages} ->
+        messages
+        |> Enum.each(fn %{metadata: %{test_pid: test_pid}} = message ->
+          Process.send(test_pid, {k, message}, [])
+        end)
+      end)
+    end
+
+    def handle_call(:get_state, _, init_arg), do: {:reply, init_arg, [], init_arg}
+    def handle_cast({:push_message, message}, init_arg), do: {:noreply, [message], init_arg}
+  end
+
+  defmodule Consumer do
+    use Lepus.Consumer,
+      exchange: "test_exchange",
+      routing_key: "test_routing_key",
+      rabbit_client: RabbitClientMock,
+      broadway_producer_module: BroadwayProducerMock
+
+    @impl Lepus.Consumer
+    def options do
+      [producer: [stages: 1], processors: [my_processor_name: [stages: 1]]]
+    end
+
+    def handle_message(_, %{metadata: %{test_pid: test_pid}, data: data} = message, _) do
+      message =
+        if data in ["Failed!", %{"str" => "Failed!"}] do
+          message |> Broadway.Message.failed(":(")
+        else
+          message
+        end
+
+      Process.send(test_pid, {:message_handled, message}, [])
+      message
+    end
+  end
+
+  describe ".start_link/1" do
+    setup do
+      {:ok, rabbit_client} = RabbitClientMock.start_link()
+      {:ok, consumer} = Consumer.start_link(connection: rabbit_client, name: new_consumer_name())
+      log = rabbit_client |> RabbitClientMock.get_log()
+
+      {:ok, log: log, rabbit_client: rabbit_client, consumer: consumer}
+    end
+
+    test "declares exchanges and queues", %{log: log, rabbit_client: rabbit_client} do
+      assert [
+               open_connection: [^rabbit_client],
+               open_channel: [connection],
+               declare_direct_exchange: [channel, "test_exchange", [durable: true]],
+               declare_direct_exchange: [channel, "test_exchange.delay", [durable: true]],
+               declare_direct_exchange: [channel, "test_exchange.retry", [durable: true]],
+               declare_queue: [channel, "test_exchange.test_routing_key", [durable: true]],
+               declare_queue: [
+                 channel,
+                 "test_exchange.test_routing_key.retry",
+                 [
+                   durable: true,
+                   arguments: [{"x-dead-letter-exchange", :longstr, "test_exchange.retry"}]
+                 ]
+               ],
+               bind_queue: [
+                 channel,
+                 "test_exchange.test_routing_key",
+                 "test_exchange",
+                 [routing_key: "test_routing_key"]
+               ],
+               bind_queue: [
+                 channel,
+                 "test_exchange.test_routing_key",
+                 "test_exchange.retry",
+                 [routing_key: "test_routing_key"]
+               ],
+               bind_queue: [
+                 channel,
+                 "test_exchange.test_routing_key.retry",
+                 "test_exchange.delay",
+                 [routing_key: "test_routing_key"]
+               ],
+               close_channel: [channel],
+               close_connection: [connection]
+             ] = log
+    end
+
+    test "starts broadway with proper options", %{
+      consumer: consumer,
+      rabbit_client: rabbit_client
+    } do
+      broadway_opts =
+        consumer
+        |> Broadway.producer_names()
+        |> List.first()
+        |> GenStage.call(:get_state)
+        |> Keyword.fetch!(:broadway)
+
+      processor_opts =
+        broadway_opts |> Keyword.fetch!(:processors) |> Keyword.fetch!(:my_processor_name)
+
+      assert Keyword.fetch!(processor_opts, :stages) == 1
+
+      producer_opts = broadway_opts |> Keyword.fetch!(:producer)
+
+      assert producer_opts |> Keyword.fetch!(:transformer) == {Lepus.Consumer, :transform, []}
+
+      assert {BroadwayProducerMock, producer_module_opts} =
+               producer_opts |> Keyword.fetch!(:module)
+
+      assert producer_module_opts |> Keyword.fetch!(:connection) == rabbit_client
+      assert producer_module_opts |> Keyword.fetch!(:on_failure) == :ack
+      assert producer_module_opts |> Keyword.fetch!(:queue) == "test_exchange.test_routing_key"
+
+      assert producer_module_opts |> Keyword.fetch!(:metadata) == [
+               :headers,
+               :routing_key,
+               :content_type,
+               :timestamp
+             ]
+    end
+  end
+
+  describe ".handle_message/3" do
+    setup do
+      {:ok, rabbit_client} = RabbitClientMock.start_link()
+      {:ok, consumer} = Consumer.start_link(connection: rabbit_client, name: new_consumer_name())
+      [producer] = consumer |> Broadway.producer_names()
+
+      {:ok, producer: producer}
+    end
+
+    test "handles message", %{producer: producer} do
+      producer |> BroadwayProducerMock.push_message("Hello!")
+      assert_receive {:message_handled, %{data: "Hello!"}}
+      assert_receive {:successful, %{data: "Hello!"}}
+    end
+
+    test "handles JSON message", %{producer: producer} do
+      producer
+      |> BroadwayProducerMock.push_message(~s({"str":"value","int":1}), %{
+        content_type: "application/json"
+      })
+
+      parsed_data = %{"str" => "value", "int" => 1}
+
+      assert_receive {:message_handled, %{data: ^parsed_data}}
+      assert_receive {:successful, %{data: ^parsed_data}}
+    end
+
+    test "reschedules failed message", %{producer: producer} do
+      {:ok, rabbit_client} = RabbitClientMock.start_link()
+
+      producer
+      |> BroadwayProducerMock.push_message(
+        "Failed!",
+        %{routing_key: "my_routing_key"},
+        rabbit_client
+      )
+
+      assert_receive {:message_handled, %{data: "Failed!"}}
+      assert_receive {:failed, %{data: "Failed!"}}
+
+      assert [
+               open_channel: _connection,
+               publish: [channel, "test_exchange.delay", "my_routing_key", "Failed!", options],
+               close_channel: [channel]
+             ] = RabbitClientMock.get_log(rabbit_client)
+
+      assert options |> Keyword.fetch!(:expiration) == "2000"
+      assert options |> Keyword.fetch!(:headers) == [{"x-retries", :long, 1}]
+      assert options |> Keyword.fetch!(:routing_key) == "my_routing_key"
+    end
+
+    test "reschedules failed JSON message", %{producer: producer} do
+      {:ok, rabbit_client} = RabbitClientMock.start_link()
+
+      payload = ~s({"str":"Failed!"})
+
+      producer
+      |> BroadwayProducerMock.push_message(
+        payload,
+        %{routing_key: "my_routing_key", content_type: "application/json"},
+        rabbit_client
+      )
+
+      assert_receive {:message_handled, %{data: %{"str" => "Failed!"}}}
+      assert_receive {:failed, %{data: %{"str" => "Failed!"}}}
+
+      assert [
+               open_channel: _connection,
+               publish: [channel, "test_exchange.delay", "my_routing_key", ^payload, options],
+               close_channel: [channel]
+             ] = RabbitClientMock.get_log(rabbit_client)
+
+      assert options |> Keyword.fetch!(:expiration) == "2000"
+      assert options |> Keyword.fetch!(:headers) == [{"x-retries", :long, 1}]
+      assert options |> Keyword.fetch!(:routing_key) == "my_routing_key"
+    end
+
+    test "reschedules failed message using exp backoff", %{producer: producer} do
+      {:ok, rabbit_client} = RabbitClientMock.start_link()
+
+      producer
+      |> BroadwayProducerMock.push_message(
+        "Failed!",
+        %{routing_key: "my_routing_key", headers: [{"x-retries", :long, 3}]},
+        rabbit_client
+      )
+
+      assert_receive {:message_handled, %{data: "Failed!"}}
+      assert_receive {:failed, %{data: "Failed!"}}
+
+      assert [
+               open_channel: _connection,
+               publish: [channel, "test_exchange.delay", "my_routing_key", "Failed!", options],
+               close_channel: [channel]
+             ] = RabbitClientMock.get_log(rabbit_client)
+
+      assert options |> Keyword.fetch!(:expiration) == "16000"
+      assert options |> Keyword.fetch!(:headers) == [{"x-retries", :long, 4}]
+      assert options |> Keyword.fetch!(:routing_key) == "my_routing_key"
+    end
+  end
+
+  defp new_consumer_name do
+    :"consumer-#{System.unique_integer([:positive, :monotonic])}"
+  end
+end
