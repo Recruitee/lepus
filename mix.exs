670bebd095d348a0d8d8a7ebe8205690e7f4341c
Serge Karpiesz <serge.karpiesz@recruitee.com>
2020-02-17 07:40:14 +0100

    Allow elixir 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Lepus.MixProject do
     [
       app: :lepus,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
